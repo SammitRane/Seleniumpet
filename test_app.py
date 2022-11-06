ba807d943aee138fc154cf5e1b1056fc4482c504
sammitRane <rane.sammit@gmail.com>
2022-11-06 22:59:12 +0530

    Misc change

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ import sys
 import core.main as app
 from selenium.webdriver.common.by import By
 
-#This test case will test the initialization of chrome:wqdriver
+#This test case will test the initialization of chromedriver
 def test_initdriver():
     driver = app.initializeChromeDriver()
     assert driver is not None
