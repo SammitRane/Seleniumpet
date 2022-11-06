9bf43cc997ebe11bd9754bf9c70d828ac26335a5
sammitRane <rane.sammit@gmail.com>
2022-11-06 22:29:22 +0530

    Added comments in the test_app.p file

--- test_app.py
+++ test_app.py
@@ -3,19 +3,19 @@ import sys
 import core.main as app
 from selenium.webdriver.common.by import By
 
-
+#This test case will test the initialization of chrome driver
 def test_initdriver():
     driver = app.initializeChromeDriver()
     assert driver is not None
 
-
+#This test case will test whether we are able to reach the website 
 def test_getwebsite():
     url = "https://www.saucedemo.com/"
     driver = app.initializeChromeDriver()
     app.getwebsite(driver, url)
     assert driver.find_element(By.ID, "root") is not None
 
-
+#This test case will check the login and logout feature
 def test_loginlogout():
     url = "https://www.saucedemo.com/"
     creds_data = app.getdata("./core/input/creds.json")
@@ -24,13 +24,13 @@ def test_loginlogout():
     app.loginlogout(driver, creds_data["username"], creds_data["password"])
     assert driver.find_element(By.ID, "root") is not None
 
-
+#This test case will check the sorting feature
 def test_sort():
     driver = setup()
     app.sortitems(driver)
     assert driver.find_element(By.ID, "root") is not None
 
-
+#This test case will select all the items and check them out
 def test_selectallcheckout():
     driver = setup()
     items = app.getdata("./core/input/items.json")
@@ -39,7 +39,7 @@ def test_selectallcheckout():
     app.checkout(driver, checkout_details)
     assert driver.find_element(By.ID, "back-to-products") is not None
 
-
+#This method is used to create a basic setup for all the test cases with some predefined steps
 def setup():
     try:
         url = "https://www.saucedemo.com/"
