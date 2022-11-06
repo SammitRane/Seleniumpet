b70424b3b63f93eef7f15fe79e5d1cfbd5458405
sammitRane <rane.sammit@gmail.com>
2022-11-06 22:56:01 +0530

    Removed the

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ import sys
 import core.main as app
 from selenium.webdriver.common.by import By
 
-#This test case will test the initialization of the chrome driver
+#This test case will test the initialization of chrome:wqdriver
 def test_initdriver():
     driver = app.initializeChromeDriver()
     assert driver is not None
